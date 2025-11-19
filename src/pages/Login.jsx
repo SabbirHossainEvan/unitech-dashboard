593e27969703f72716905ca4017f123a3034077b
SabbirHossainEvan <evansabbir31@gmail.com>
2025-11-19 18:03:07 +0600

    login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,73 +1,75 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import logoImage from "../assets/Frame 45.png"
 
 export default function Login() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [role, setRole] = useState("Instructor");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [role, setRole] = useState("Instructor");
 
-  const handleLogin = (e) => {
-    e.preventDefault();
+    const handleLogin = (e) => {
+        e.preventDefault();
 
-    // Fake authentication logic (you can replace with API)
-    if (!username || !password) {
-      alert("Please fill all fields!");
-      return;
-    }
+        if (!username || !password) {
+            alert("Please fill all fields!");
+            return;
+        }
 
-    // Redirect based on role
-    if (role === "Admin") {
-      navigate("/admin");
-    } else if (role === "Instructor") {
-      navigate("/instructor");
-    } else if (role === "Student") {
-      navigate("/student");
-    }
-  };
+        if (role === "Admin") {
+            navigate("/admin");
+        } else if (role === "Instructor") {
+            navigate("/instructor");
+        } else if (role === "Student") {
+            navigate("/student");
+        }
+    };
 
-  return (
-    <div className="w-full h-screen flex justify-center items-center bg-gray-50">
-        <div>
-            <img src="" alt="" />
+    return (
+        <div className="w-full h-screen flex justify-center items-center bg-gray-50">
+            <div>
+                <div>
+                    <img src={logoImage} alt="" className="block mx-auto" />
+                </div>
+                <form
+                    onSubmit={handleLogin}
+                    className="w-[400px] p-8 rounded-xl"
+                >
+                    <h2 className="text-3xl font-bold mb-8">Welcome Back 👋</h2>
+
+                    <label className="text-md text-gray-400">Username or Phone</label>
+                    <input
+                        type="text"
+                        placeholder="Username or Phone"
+                        className="w-full text-sm border p-2 rounded-lg border-gray-300 mb-3 my-2"
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+
+                    <label className="text-md text-gray-400">Password</label>
+                    <input
+                        type="password"
+                        placeholder="*********"
+                        className="w-full text-sm border p-2 rounded-lg border-gray-300 mb-3 my-2"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    <label className="text-md text-gray-400">I'm a</label>
+                    <select
+                        className="w-full text-sm border p-2 rounded-lg border-gray-300 mb-3 my-2"
+                        onChange={(e) => setRole(e.target.value)}
+                    >
+                        <option>Instructor</option>
+                        <option>Admin</option>
+                        <option>Student</option>
+                    </select>
+
+                    <button className="w-full bg-orange-400 text-white p-2 rounded">
+                        Login
+                    </button>
+                </form>
+            </div>
+
         </div>
-      <form
-        onSubmit={handleLogin}
-        className="w-[400px] p-8 rounded-xl"
-      >
-        <h2 className="text-3xl font-bold mb-4">Welcome Back 👋</h2>
-
-        <label className="text-sm font-semibold">Username or Phone</label>
-        <input
-          type="text"
-          placeholder="username"
-          className="w-full border p-2 rounded mb-3"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-
-        <label className="text-sm font-semibold">Password</label>
-        <input
-          type="password"
-          placeholder="*********"
-          className="w-full border p-2 rounded mb-3"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <label className="text-sm font-semibold">I'm a</label>
-        <select
-          className="w-full border p-2 rounded mb-4"
-          onChange={(e) => setRole(e.target.value)}
-        >
-          <option>Instructor</option>
-          <option>Admin</option>
-          <option>Student</option>
-        </select>
-
-        <button className="w-full bg-orange-500 text-white p-2 rounded">
-          Login
-        </button>
-      </form>
-    </div>
-  );
+    );
 }
