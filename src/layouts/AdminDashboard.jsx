2099fa5980215afd7b5e2cbcad14cef8e6929501
SabbirHossainEvan <evansabbir31@gmail.com>
2025-11-26 14:46:11 +0600

    instructor dashboard

--- src/layouts/AdminDashboard.jsx
+++ src/layouts/AdminDashboard.jsx
@@ -6,15 +6,17 @@ export default function AdminDashboard() {
   return (
     <div>
       <div className="flex">
+        {/* side navbar */}
         <section className="sticky top-0 h-fit">
           <SideNavbar></SideNavbar>
         </section>
+        {/* top navbar & main content */}
         <section className="w-full mx-auto">
           {/* top navbar */}
           <section className="sticky top-0 h-fit">
             <TopNavbar></TopNavbar>
           </section>
-
+          {/* main content */}
           <section>
             <Outlet>
             </Outlet>
--- src/layouts/InstructorDashboard.jsx
+++ src/layouts/InstructorDashboard.jsx
@@ -1,7 +1,25 @@
+import { Outlet } from "react-router";
+import TopNavbar from "../adminComponents/TopNavbar";
+import InstructorSideNavbar from "../instructorComponent/InstructorSideNavBar";
+
 export default function InstructorDashboard() {
   return (
-    <div className="p-10">
-      <h1 className="text-3xl font-bold">Instructor Dashboard</h1>
+    <div className="flex">
+              {/* side navbar */}
+      <section className="sticky top-0 h-fit">
+        <InstructorSideNavbar></InstructorSideNavbar>
+      </section>
+      {/* top navbar & main content */}
+      <section className="w-full mx-auto">
+        {/* top navbar */}
+        <section className="sticky top-0 h-fit">
+          <TopNavbar></TopNavbar>
+        </section>
+        {/* main content */}
+        <section>
+          <Outlet></Outlet>
+        </section>
+      </section>
     </div>
   );
 }
