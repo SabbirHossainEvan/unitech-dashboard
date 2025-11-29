e081f8a19875e9b3aa125c44e5ed2554ffaf7063
SabbirHossainEvan <evansabbir31@gmail.com>
2025-11-29 23:55:51 +0600

    filter section update

--- src/adminComponents/FilterSearchSection.jsx
+++ src/adminComponents/FilterSearchSection.jsx
@@ -64,7 +64,7 @@ const FilterSearchSection = ({ onSearch, onReset }) => {
       {/* Reset Button */}
       <button
         onClick={handleReset}
-        className="w-full bg-white text-gray-700 py- px-4 border border-gray-300 rounded-md text-base font-semibold hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-200 focus:ring-opacity-75 transition duration-150 ease-in-out flex items-center justify-center space-x-2"
+        className="w-full bg-white text-gray-700 py-2 px-4 border border-gray-300 rounded-md text-base font-semibold hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-200 focus:ring-opacity-75 transition duration-150 ease-in-out flex items-center justify-center space-x-2"
       >
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.9 6.401V10a1 1 0 01-2 0V3a1 1 0 011-1zm10 14a1 1 0 01-1-1v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 111.885-.666A5.002 5.002 0 0014.1 13.599V10a1 1 0 012 0v7a1 1 0 01-1 1z" clipRule="evenodd" />
